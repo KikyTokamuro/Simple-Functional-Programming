0a78de9c9362a0e14edc8e922d58bf3ea9b0493a
Daniil A <kiky.tokamuro@yandex.ru>
2019-01-20 13:26:37 +0300

    Add scheme example
    - partial application

---
+++ examples-scheme/partial-application.scm
@@ -0,0 +1,23 @@
+(define (partial fun . args)
+  (lambda x (apply fun (append args x))))
+
+
+;; Обычная функция принимающая 3 аргумента:
+(define (f x y z)
+  (* (+ x y z) 100))
+
+(display (f 2 3 4)) ;; -> 900
+(newline)
+
+
+;; Частичное применение этой же функции:
+(define (funcYZ y z)
+  ((partial f 2) y z)) ;; x = 2
+
+(display (funcYZ 3 4)) ;; -> 900
+(newline)
+
+(display ((partial f 2) 3 4)) ;; 900
+(newline)
+
+
